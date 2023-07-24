ca5e31f5f65a6b2dc64b51d50255e09343a56fad
vc-maitrichampaneri <Maitri.Champaneri@dpworld.com>
2023-07-24 17:59:34 +0530

    init setup

---
+++ src/mock/mock.js
@@ -0,0 +1,44 @@
+export const headerMenuList = [
+  {
+    title: "Work",
+    url: "/work",
+    icon: "work.svg"
+  },
+  {
+    title: "Resume",
+    url: "/resume",
+    icon: "resume.svg"
+  },
+  {
+    title: "Shelf",
+    url: "/shelf",
+    icon: "shelf.svg"
+  },
+]
+
+export const skillSet = [
+  {
+    name: "HTML",
+    icon: "",
+  },
+  {
+    name: "CSS",
+    icon: "",
+  },
+  {
+    name: "JavaScript",
+    icon: "",
+  },
+  {
+    name: "jQuery",
+    icon: "",
+  },
+  {
+    name: "TailWind",
+    icon: "",
+  },
+  {
+    name: "Bootstrap",
+    icon: "",
+  },
+];
